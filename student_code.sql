a08890b712f5ebf9d1ed66af9d7eca61466d71f2
vinothini149 <vinothiniav1409@gmail.com>
2026-09-24 13:40:58 +0530

    Program 18

--- student_code.sql
+++ student_code.sql
@@ -1,1 +1,13 @@
+CREATE OR REPLACE FUNCTION count_students_in_dept (
+    p_dept IN VARCHAR2
+) RETURN NUMBER IS
+    v_count NUMBER := 0;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM Student
+    WHERE department = p_dept;
 
+    RETURN v_count;
+END count_students_in_dept;
+/
